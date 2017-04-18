9679bd866a7ccafbaaefe8b658f6e44896dc5a84
ian shirley <ian shirley>
2017-04-18 10:38:24 -0500

    font file added

---
+++ tests/config.js
@@ -0,0 +1,11 @@
+// fbgraph config info
+var conf = {
+    client_id:      '1858193971063846'
+  , client_secret:  '745405be9615ebc03aafe1c56a858aa7'
+  , scope:          'email, user_about_me, user_friends'
+  // You have to set http://localhost:3000/ as your website
+  // using Settings -> Add platform -> Website
+  , redirect_uri:   'http://localhost:3000/auth'
+};
+
+module.exports = conf;
